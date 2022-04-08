3a0289c5f3068e7217b5af721bdc28824f86911d
Quickdesh <fyfrive@gmail.com>
2022-04-08 18:22:11 +0900

    Update to v0.12.7.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,7 +30,7 @@ android {
         minSdk = AndroidConfig.minSdk
         targetSdk = AndroidConfig.targetSdk
         versionCode = 72
-        versionName = "0.12.7.0"
+        versionName = "0.12.7.1"
 
         buildConfigField("String", "COMMIT_COUNT", "\"${getCommitCount()}\"")
         buildConfigField("String", "COMMIT_SHA", "\"${getGitSha()}\"")
