b9b07f1ef2d83462f44d7aed6e5125b569411499
Quickdesh <fyfrive@gmail.com>
2022-07-11 22:29:48 +0530

    delete uselss manga files

--- app/src/main/java/eu/kanade/presentation/manga/components/DotSeparatorText.kt
+++
@@ -1,9 +0,0 @@
-package eu.kanade.presentation.manga.components
-
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-
-@Composable
-fun DotSeparatorText() {
-    Text(text = " • ")
-}
