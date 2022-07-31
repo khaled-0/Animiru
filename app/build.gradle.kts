e9f14e803bc097e1c3a30bea36362d80791242f0
Quickdesh <fyfrive@gmail.com>
2022-07-31 14:44:21 +0530

    update to v0.13.3.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
         minSdk = AndroidConfig.minSdk
         targetSdk = AndroidConfig.targetSdk
         versionCode = 81
-        versionName = "0.13.3.0"
+        versionName = "0.13.3.1"
 
         buildConfigField("String", "COMMIT_COUNT", "\"${getCommitCount()}\"")
         buildConfigField("String", "COMMIT_SHA", "\"${getGitSha()}\"")
