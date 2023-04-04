59241e2ebfe1d65a53cd2d0c9fcf7807c238dfb4
Quickdesh <devesh.ratra@gmail.com>
2023-04-03 22:28:47 -0400

    update to v0.14.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,7 +30,7 @@ android {
         minSdk = AndroidConfig.minSdk
         targetSdk = AndroidConfig.targetSdk
         versionCode = 94
-        versionName = "0.14.1.1"
+        versionName = "0.14.1.2"
 
         buildConfigField("String", "COMMIT_COUNT", "\"${getCommitCount()}\"")
         buildConfigField("String", "COMMIT_SHA", "\"${getGitSha()}\"")
