2163bbaa163b149463e8cd1301d3a62849df1200
Quickdesh <devesh.ratra@gmail.com>
2023-08-03 16:16:19 +0530

    update to v0.15.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         applicationId = "xyz.Quickdev.Animiru.mi"
 
         versionCode = 103
-        versionName = "0.15.1.1"
+        versionName = "0.15.1.2"
 
         buildConfigField("String", "COMMIT_COUNT", "\"${getCommitCount()}\"")
         buildConfigField("String", "COMMIT_SHA", "\"${getGitSha()}\"")
